284e49b50ff76a45d823b7b6625712ef2ceee3f6
Alexander Kirakozov <akirakozov@yandex-team.ru>
2016-10-13 21:25:14 +0300

    Add some bad code project for refactoring

---
+++ java/refactoring/src/main/java/ru/akirakozov/sd/refactoring/servlet/AddProductServlet.java
@@ -0,0 +1,37 @@
+package ru.akirakozov.sd.refactoring.servlet;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+/**
+ * @author akirakozov
+ */
+public class AddProductServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String name = request.getParameter("name");
+        long price = Long.parseLong(request.getParameter("price"));
+
+        try {
+            try (Connection c = DriverManager.getConnection("jdbc:sqlite:test.db")) {
+                String sql = "INSERT INTO PRODUCT " +
+                        "(NAME, PRICE) VALUES (\"" + name + "\"," + price + ")";
+                Statement stmt = c.createStatement();
+                stmt.executeUpdate(sql);
+                stmt.close();
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        response.setContentType("text/html");
+        response.setStatus(HttpServletResponse.SC_OK);
+        response.getWriter().println("OK");
+    }
+}
