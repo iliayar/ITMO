ac75984d20efc17890dbdf8c0bc6d149a7e86840
Alexander Kirakozov <akirakozov@yandex-team.ru>
2016-10-22 00:45:21 +0300

    Add jdbc dao with sqllite

---
+++ java/mvc/src/main/java/ru/akirakozov/sd/mvc/dao/ProductJdbcDao.java
@@ -0,0 +1,43 @@
+package ru.akirakozov.sd.mvc.dao;
+
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.support.JdbcDaoSupport;
+import ru.akirakozov.sd.mvc.model.Product;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * @author akirakozov
+ */
+public class ProductJdbcDao extends JdbcDaoSupport implements ProductDao {
+    @Override
+    public int addProduct(Product product) {
+        String sql = "INSERT INTO PRODUCT (NAME, PRICE) VALUES (?, ?)";
+        return getJdbcTemplate().update(sql, new Object[] { product.getName(), product.getPrice()});
+    }
+
+    @Override
+    public List<Product> getProducts() {
+        String sql = "SELECT * FROM PRODUCT";
+        return getProductsByRequest(sql);
+    }
+
+    @Override
+    public Optional<Product> getProductWithMaxPrice() {
+        String sql = "SELECT * FROM PRODUCT ORDER BY PRICE DESC LIMIT 1";
+        return getProductsByRequest(sql).stream().findFirst();
+    }
+
+    @Override
+    public Optional<Product> getProductWithMinPrice() {
+        String sql = "SELECT * FROM PRODUCT ORDER BY PRICE LIMIT 1";
+        return getProductsByRequest(sql).stream().findFirst();
+
+    }
+
+    private List<Product> getProductsByRequest(String sql) {
+        return getJdbcTemplate().query(sql, new BeanPropertyRowMapper(Product.class));
+    }
+
+}
