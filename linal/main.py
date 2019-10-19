db2fefd33da12ec21ba283c9a7bfb4ab2a9776d6
iliayar <iliayar3@gmail.com>
2019-10-19 02:21:53 +0000

    linal lab1 hzzz

--- linal/main.py
+++ linal/main.py
@@ -65,10 +65,13 @@ class Matrix(object):
         return (len(self.matrix),len(self.matrix[0]))
 
     @staticmethod
-    def parseMatrix(n,m,string,type):
-        lst = list(map(type,string.split()))
+    def parseMatrix(n,m,string,t):
+        if type(string) == list:
+            lst = string
+        else:
+            lst = list(map(t,string.split()))
 
-        return Matrix([lst[i:i+m] for i in range(0,n*m,m)],type)
+        return Matrix([lst[i:i+m] for i in range(0,n*m,m)],t)
 
     @staticmethod
     def canMultiply(a,b):
@@ -85,38 +88,76 @@ class Matrix(object):
 
 
 with open("input.txt","r") as inp:
-    def readMatrix(n,m):
-        s = inp.readline()
-        # while(len(s.split()) < n*m):
-        #     s += " " + inp.readline()
-        return s
-
-    a, b = map(float,inp.readline().split())
-
-    na, ma = map(int,inp.readline().split())
-    A = Matrix.parseMatrix(na,ma,readMatrix(na,ma),float)
-
-    nb, mb = map(int,inp.readline().split())
-    B = Matrix.parseMatrix(nb,mb,readMatrix(nb,mb),float)
-
-    nc, mc = map(int,inp.readline().split())
-    C = Matrix.parseMatrix(nc,mc,readMatrix(nc,mc),float)
-
-    nd, md = map(int,inp.readline().split())
-    D = Matrix.parseMatrix(nd,md,readMatrix(nd,md),float)
-
-    nf, mf = map(int,inp.readline().split())
-    F = Matrix.parseMatrix(nf,mf,readMatrix(nf,mf),float)
-
-    # print(C*(A*a + B.transpose()*b)*D)
-
+    # def readMatrix(n,m):
+    #     s = inp.readline()
+    #     while(len(s.split()) < n*m):
+    #         s += " " + inp.readline()
+    #     return s
     try:
+
+        inAll = list(map(float,inp.read().split()))
+
+        # a, b = map(float,inp.readline().split())
+        a ,b = inAll[:2]
+        inAll = inAll[2:]
+
+
+        # na, ma = map(int,inp.readline().split())
+        na ,ma = map(int,inAll[:2])
+        assert(na != 0 and ma != 0)
+        inAll = inAll[2:]
+        # A = Matrix.parseMatrix(na,ma,readMatrix(na,ma),float)
+        A = Matrix.parseMatrix(na,ma,inAll[:na*ma],float)
+        inAll = inAll[na*ma:]
+
+        # nb, mb = map(int,inp.readline().split())
+        nb ,mb = map(int,inAll[:2])
+        assert(nb != 0 and mb != 0)
+        inAll = inAll[2:]
+
+        # B = Matrix.parseMatrix(nb,mb,readMatrix(nb,mb),float)
+        B = Matrix.parseMatrix(nb,mb,inAll[:nb*mb],float)
+        inAll = inAll[nb*mb:]
+
+
+        # nc, mc = map(int,inp.readline().split())
+        nc ,mc = map(int,inAll[:2])
+        assert(nc != 0 and mc != 0)
+        inAll = inAll[2:]
+        
+        # print(inAll)
+        # C = Matrix.parseMatrix(nc,mc,readMatrix(nc,mc),float)
+        C = Matrix.parseMatrix(nc,mc,inAll[:nc*mc],float)
+        inAll = inAll[nc*mc:]
+
+
+        # nd, md = map(int,inp.readline().split())
+        nd ,md = map(int,inAll[:2])
+        assert(nd != 0 and md != 0)
+        inAll = inAll[2:]
+        
+        # D = Matrix.parseMatrix(nd,md,readMatrix(nd,md),float)
+        D = Matrix.parseMatrix(nd,md,inAll[:nd*md],float)
+        inAll = inAll[nd*md:]
+
+        # nf, mf = map(int,inp.readline().split())
+        nf ,mf = map(int,inAll[:2])
+        assert(nf != 0 and mf != 0)
+        inAll = inAll[2:]
+        
+        # F = Matrix.parseMatrix(nf,mf,readMatrix(nf,mf),float)
+        F = Matrix.parseMatrix(nf,mf,inAll[:nf*mf],float)
+        inAll = inAll[nf*mf:]
+
+        # print(D)
+        # print(C*(A*a + B.transpose()*b)*D)
+
         res = C*(A*a + B.transpose()*b).transpose()*D + F*(-1)
         with open("output.txt","w") as out:
-            out.write("1\n")            
-            out.write(str(res.shape()[0]) + " " + str(res.shape()[1]) + "\n")
-            out.write(str(res) + "\n")
+            out.write("1 ")            
+            out.write(str(res.shape()[0]) + " " + str(res.shape()[1]) + " ")
+            out.write(str(res)+"\n\n")
     except:
         with open("output.txt","w") as out:
-            out.write("0\n")            
+            out.write("0\n\n")            
     
