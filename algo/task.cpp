4771cd8f2242e7bcd4382cb7e0ebc6e6b0091c90
iliayar <iliayar3@gmail.com>
2019-10-28 19:56:58 +0300

    Gnome D algo

--- algo/task.cpp
+++ algo/task.cpp
@@ -1,48 +1,96 @@
 #include <iostream>
-#include <fstream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <cmath>
 
+#define int long long
 
 using namespace std;
 
-int arr[3000001];
-
-int q_sort(int left, int right, int knumb){
-    int  center = arr[(left + right) / 2];
-    int i = left;
-    int j = right - 1;
-    while (i <= j){
-        while (arr[i] < center){
-            i++;
-        }
-        while (arr[j] > center){
-            j--;
-        }
-        if (i <= j){
-            swap(arr[i], arr[j]);
-            i++;
-            j--;
-
-        }
-
+struct Gnome {
+    int id;
+    Gnome* next;
+    Gnome* prev;
+    Gnome() {
+        this->id = -1;
+        this->prev = NULL;
+        this->next = NULL;
     }
-    if (i == knumb) return arr[i];
-    if (i > knumb){
-        return q_sort(left, i, knumb);
-    }else{
-        return q_sort(i, right, knumb);
+    Gnome(int id, Gnome *next, Gnome *prev) {
+        this->id = id;
+        this->next = next;
+        this->prev = prev;
     }
+};
+
+int m = 0;
+
+Gnome *mid;
+Gnome *head = new Gnome();
+Gnome *tail = new Gnome();
+
+void push_normal(int x) {
+    Gnome* a = new Gnome(x,tail->next,tail);
+    tail->next->prev = a;
+    tail->next = a;
+    if(m == 0) {
+        mid = a;
+    } else if(m%2 == 0) {
+        mid = mid->prev;
+    }
+    m++;
 }
 
-int main(){
-    //ifstream cin("kth.in");
-    //ofstream cout("kth.out");
-    int a, b, c, n, k;
-    cin >> n >> k >> a >> b >> c;
-    cin >> arr[0] >> arr[1];
-    for (int i = 2; i < n; i++){
-        arr[i] = a * arr[i - 2] + b * arr[i - 1] + c;
+void push_CHAD(int x) {
+    Gnome* a = new Gnome(x, mid, mid->prev);
+    mid->prev->next = a;
+    mid->prev = a;
+    if(m%2 == 1) {
+        mid = a;
     }
-    
-    cout << q_sort(0, n, k - 1) << endl;
+    m++;
+}
+
+int pop() {
+    Gnome *res = head->prev;
+    res->prev->next = head;
+    head->prev = res->prev;
+    if(m%2 == 1) {
+        mid = mid->next;
+    }
+    m--;
+    return res->id;
+}
+
+void print_stack() {
+    Gnome *i = head->prev;
+    //while(i->id != -1) {
+    //    cout << i->id << " ";
+    //    i = i->prev;
+    //}
+    cout << i->id;
+    cout << endl;
+}
+
+signed main(){
+    head->prev = tail;
+    tail->next = head;
+//    int n; cin >> n;
+//    for(int i = 0; i < n; ++i) {
+//        char t; cin >> t;
+    char t;
+    while(cin >> t) {
+        if(t == '+') {
+            int x; cin >> x;
+            push_normal(x);
+        } else if(t == '*') {
+            int x; cin >> x;
+            push_CHAD(x);
+        } else {
+            cout << pop() << endl;
+        }
+    }
+
     return 0;
 }
