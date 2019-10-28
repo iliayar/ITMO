2fd6f7649eae87525cf75ec966569dc3546d903b
iliayar <iliayar3@gmail.com>
2019-10-28 21:41:34 +0000

    Discrete lab1

--- Discrete/task.cpp
+++ Discrete/task.cpp
@@ -1,206 +1,61 @@
 #include <iostream>
+//#include <bits/stdc++.h>
 #include <vector>
-#include <string>
-#include <map>
-#include <algorithm>
-#include <cmath>
  
 #define int long long
  
 using namespace std;
  
-class BinArray {
-    unsigned int number;
+vector<int> a;
  
-public:
-    BinArray() {
-        BinArray(0);
+int n = 0;
+ 
+void insert(int x) {
+    a[n++] = x;
+ 
+    for(int i  = n -1; i > 0 && a[(i-1)/2] > a[i]; i = (i-1)/2) {
+        swap(a[i],a[(i-1)/2]);
     }
- 
-    BinArray(unsigned int number) {
-        this->number = number;
-    }
- 
-    static BinArray fromString(const string& strNumber) {
-        unsigned int number = 0;
-        for(int i = 0; i < strNumber.length(); ++i) {
-            number += ((strNumber[i] - '0') << i);
-        }
-        return BinArray(number);
-    }
-     
-    static BinArray fromReverseString(const string& strNumber) {
-        unsigned int number = 0;
-        for(int i = 0; i < strNumber.length(); ++i) {
-            number = number*2 + (strNumber[i] - '0');
-        }
-        return BinArray(number);
-    }
- 
-    static BinArray fromVector(const vector<bool>& strVector) {
-        unsigned int number = 0;
-        for(int i = 0; i < strVector.size(); ++i) {
-            number += strVector[i] << i;
-        }
-        return BinArray(number);
-    }
- 
-    static BinArray fromReverseVector(const vector<bool>& strVector) {
-        unsigned int number = 0;
-        for(int i = 0; i < strVector.size(); ++i) {
-            number = number*2 + strVector[i];
-        }
-        return BinArray(number);
-    }
- 
-    void set(int i, bool val) {
-        if(val == 0) {
-            this->number = this->number & ~(1 << i);
-        } else {
-            this->number = this->number | (1 << i);
-        }
-    }
- 
-    bool operator [](int i) const {
-        return (bool)(1 & (number >> i));
-    }
- 
-    BinArray operator &(unsigned int a) const {
-        return BinArray(this->number & a);
-    }
- 
-    BinArray operator |(unsigned int a) const {
-        return BinArray(this->number | a);
-    }
- 
-    BinArray operator ^(unsigned int a) const {
-        return BinArray(this->number ^ a);
-    }
- 
-    unsigned int value() {
-        return this->number;
-    }
-    unsigned int revValue() {
-        int number = this->number;
-        int res = 0;
-        while(number > 0) {
-            res = res + number%2;
-            number /= 2;
-        }
-        return res;
-    }
- 
- 
-    BinArray operator ++(signed i) {
-        this->number++;
-        return *this;
-    }
- 
-    bool operator <(int n) const {
-        return this->number < n;
-    }
- 
-    string toString(int n) {
-        string res = "";
-        int number = this->number;
-        for(int i = 0; i < n; ++i) {
-            res += to_string(number%2);
-            number /= 2;
-        }
-        return res;
-    }
- 
-};
- 
-vector<BinArray> a;
- 
-string buildExprSDNF(BinArray line) {
-    string res = "";
-    res += 40;
-    for(int i = 0; i < a.size(); ++i) {
-        res += 40;
-        if(line[i] == 0)
-            res += 126;
-        res += to_string(i+1);
-        res += 41;
-        if(i != a.size() - 1) {
-            res += 38;
-        }
-    }
-    res += 41;
-    return res;
 }
  
-string buildExprSKNF(BinArray line) {
-    string res = "";
-    res += 40;
-    for(int i = 0; i < a.size(); ++i) {
-        res += 40;
-        if(line[i] == 1)
-            res += 126;
-        res += to_string(i+1);
-        res += 41;
-        if(i != a.size() - 1) {
-            res += 124;
+int extract() {
+    swap(a[0],a[--n]);
+    int i = 0;
+    while(true) {
+        if(2*i + 1 < n && a[2*i + 1] < a[i]) {
+            if(2*i + 2 < n && a[2*i + 2] < a[2*i + 1]) {
+                swap(a[i],a[2*i + 2]);
+                i = 2*i + 2;
+            } else {
+                swap(a[i],a[2*i + 1]);
+                i = 2*i + 1;
+            }
+        }
+        else {
+            if(2*i + 2 < n && a[2*i + 2] < a[i]) {
+                swap(a[i],a[2*i + 2]);
+                i = 2*i + 2;
+            } else
+                break;
         }
     }
-    res += 41;
-    return res;
+    return a[n];
 }
  
 signed main() {
-    int n; cin >> n;
-    a.resize(n);
-    int maxP = 0;
-    for(int i = 0; i < n; ++i) {
-        unsigned int t; cin >> t;
-        a[i] = BinArray(t);
-        int c = 0; while(t >>= 1) c++;
-        maxP = max(maxP,c);
-    }
-    unsigned int t; cin >> t;
-    BinArray s(t);
-    int c = 0; while(t >>= 1) c++;
-    maxP = max(maxP,c);
  
-    BinArray was(0);
-    BinArray table(0);
+    int m; cin >> m;
  
-    for(int i = 0; i <= maxP + 1; ++i) {
-        BinArray c(0);
-        for(int j = 0; j < n; ++j) {
-            c.set(j,a[j][i]);
-        }
-        if(was[c.value()] && (table[c.value()] != s[i])) {
-            cout << "Impossible" << endl;
-            return 0;
-        }
-        was.set(c.value(), 1);
-        table.set(c.value(),s[i]);
-    }
+    a.resize(2*m);
  
-    string res = "";
-    bool flag = false;
-    for(int i = 0; i < (1 << n); ++i) {
-        if(table[i]) {
-            if(flag)
-                res += 124;
-            res += buildExprSDNF(i);
-            flag = true;
-        }
+    for(int i = 0; i < m; ++i) {
+        int t; cin >> t;
+        insert(t);
     }
-    if(res.length() == 0) {
-        res = "";
-        bool flag = false;
-        for(int i = 0; i < (1 << n); ++i) {
-            if(!table[i]) {
-                if(flag)
-                    res += 38;
-                res += buildExprSKNF(i);
-                flag = true;
-            }
-        }
+    for(int i = 0; i < m; ++i) {
+        cout << extract() << " ";
     }
-    cout << res << endl;
+    cout << endl;
+ 
     return 0;
 }
