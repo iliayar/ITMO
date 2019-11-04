738e65f722ab2fa2a5cd17544e5872a6ccb5ecd4
iliayar <iliayar3@gmail.com>
2019-11-04 18:39:53 +0000

    java contest ended??

--- programming/contest/Main.java
+++ programming/contest/Main.java
@@ -1,42 +1,111 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.InputStream;
-import java.io.StringReader;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
-import java.io.Closeable;
-import java.io.IOException;
-import java.io.Reader;
+import java.io.*;
 
 public class Main {
+    static boolean[] was;
+    static int[] cnt;
+    static int[][] g;
+    static int[] c;
+    static int[] l;
+    static int middle = -1;
+    static int mx = -1;
+    static int mi = 0;
+
+    static void doubleG(int i) {
+        g[i] = Arrays.copyOf(g[i], g[i].length * 2);
+    }
+
     public static void main(String[] args) throws IOException {
-        Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
-        int a[][] = new int[n][n];
-        int res[][] = new int[n][n];
-        for(int i = 0; i < n; ++i) {
-            String s = in.nextString();
-            for(int j = 0; j < n; ++j) {
-                a[i][j] = (s.charAt(j) - '0');
+        MyReader in = new MyReader(System.in);
+        int n, m;
+        n = in.nextInt();
+        m = in.nextInt();
+
+        c = new int[m];
+        g = new int[n][1];
+        l = new int[n];
+
+        cnt = new int[n];
+        was = new boolean[n];
+
+        for(int i = 0; i < n - 1; ++i) {
+            int v, u;
+            v = in.nextInt(); v--;
+            u = in.nextInt(); u--;
+            if(l[v] >= g[v].length) doubleG(v);
+            if(l[u] >= g[u].length) doubleG(u);
+            g[v][l[v]] = u;
+            g[u][l[u]] = v;
+            l[v]++; l[u]++;
+        }
+        for(int i = 0; i < m; ++i) {
+            c[i] = in.nextInt() - 1;
+        }
+        count(c[0],0); renewWas();
+        for(int i = 0; i < m; ++i) {
+            if(mx < cnt[c[i]]) {
+                mx = cnt[c[i]];
+                mi = c[i];
             }
         }
-        for (int i = 0; i < n; i++) {
-            for(int j = i + 1; j < n; ++j) {
-                if(a[i][j] == 0) continue;
-                res[i][j] = 1;
-                for(int q = j + 1; q < n; ++q) {
-                    if(q == j || q == i) continue;
-                    a[i][q] = (a[i][q] - a[j][q] + 100) % 10;
+        findM(c[0], mi); renewWas();
+        if(middle != -1) {
+            count(middle, 0);
+            if(check()) {
+                System.out.println("YES");
+                System.out.println(middle + 1);
+                return;
+            }
+        }
+        System.out.println("NO");
+    }
+
+    static boolean findM(int i, int end) {
+        was[i] = true;
+        if(i == end){
+            if(cnt[i] == mx/2 && mx%2 == 0) {
+                middle = i;
+            }
+            return true;
+        }
+        for(int j = 0; j < l[i]; ++j) {
+            if(was[g[i][j]]) continue;
+            if(findM(g[i][j], end)) {
+                // System.out.println(cnt[i] + " true " + i);
+                if(cnt[i] == mx/2 && mx%2 == 0) {
+                    middle = i;
                 }
+                return true;
+            }
+            // System.out.println(cnt[i] + " false " + i);
+        }
+        return false;
+    }
+
+    static void renewWas() {
+        for(int i = 0; i < was.length; ++i) {
+            was[i] = false;
+        }
+    }
+
+    static boolean check() {
+        for(int i = 1; i < c.length; ++i) {
+            if(cnt[c[i-1]] != cnt[c[i]]) {
+                return false;
             }
         }
-        for(int i = 0; i < n; ++i) {
-            for(int j = 0; j < n; ++j) {
-                System.out.print(a[i][j]);
-            }
-            System.out.println();
+        return true;
+    }
+
+    static void count(int i, int h) {
+        cnt[i] = h;
+        was[i] = true;
+        for(int j = 0; j < l[i]; ++j) {
+            if(was[g[i][j]]) continue;
+            count(g[i][j],h+1);
         }
-        in.close();       
     }
 
 
@@ -242,5 +311,126 @@ public class Main {
         }
     
     }
+
+
+    //####################READER####################################
+    static class MyReader {
+		BufferedInputStream in;
+
+		final int bufSize = 1 << 16;
+		final byte b[] = new byte[bufSize];
+
+		MyReader( InputStream in ) {
+			this.in = new BufferedInputStream(in, bufSize);
+		}
+
+		int nextInt() throws IOException {
+			int c;
+			while ((c = nextChar()) <= 32)
+				;
+			int x = 0, sign = 1;
+			if (c == '-') {
+				sign = -1;
+				c = nextChar();
+			}
+			while (c >= '0') {
+				x = x * 10 + (c - '0');
+				c = nextChar();
+			}
+			return x * sign;
+		}
+
+		StringBuilder _buf = new StringBuilder();
+		String nextWord() throws IOException {
+			int c;
+			_buf.setLength(0);
+			while ((c = nextChar()) <= 32 && c != -1)
+				;
+			if (c == -1)
+				return null;
+			while (c > 32) {
+				_buf.append((char)c);
+				c = nextChar();
+			}
+			return _buf.toString();
+		}
+
+		int bn = bufSize, k = bufSize;
+		
+		int nextChar() throws IOException {
+			if (bn == k) {
+				k = in.read(b, 0, bufSize);
+				bn = 0;
+			}
+			return bn >= k ? -1 : b[bn++];
+		}
+
+		int nextNotSpace() throws IOException {
+			int ch;
+			while ((ch = nextChar()) <= 32 && ch != -1)
+				;
+			return ch;
+		}
+    }
+    
+    //##################WRITER######################
+    static class MyWriter {
+		BufferedOutputStream out;
+
+		final int bufSize = 1 << 16;
+		int n;
+		byte b[] = new byte[bufSize];
+
+		MyWriter( OutputStream out ) {
+			this.out = new BufferedOutputStream(out, bufSize);
+			this.n = 0;
+		}
+
+		byte c[] = new byte[20];
+		void print( int x ) throws IOException {
+			int cn = 0;
+			if (n + 20 >= bufSize)
+				flush();
+			if (x < 0) {
+				b[n++] = (byte)('-');
+				x = -x;
+			}
+			while (cn == 0 || x != 0) {
+				c[cn++] = (byte)(x % 10 + '0');
+				x /= 10;
+			}
+			while (cn-- > 0)
+				b[n++] = c[cn];
+		}
+
+		void print( char x ) throws IOException {
+			if (n == bufSize)
+				flush();
+			b[n++] = (byte)x;
+		}
+
+		void print( String s ) throws IOException {
+			for (int i = 0; i < s.length(); i++)
+				print(s.charAt(i));
+		}
+		void println( String s ) throws IOException {
+			print(s);
+			println();
+		}
+
+		static final String newLine = System.getProperty("line.separator");
+		void println() throws IOException {
+			print(newLine);
+		}
+
+		void flush() throws IOException {
+			out.write(b, 0, n);
+			n = 0;
+		}
+		void close() throws IOException {
+			flush();
+			out.close();
+		}
+	}
     
 }
