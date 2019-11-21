d2a14e18a8cfb692d08003e0f26b47593dca41f1
iliayar <iliayar3@gmail.com>
2019-11-22 02:47:32 +0300

    linal lab

---
+++ programming/HW9/md2html/Type.java
@@ -0,0 +1,16 @@
+package md2html;
+
+public enum Type {
+    OP_SQR_BRACKET,
+    CL_SQR_BRACKET,
+    OP_BRACKET,
+    CL_BRACKET,
+    APOS,
+    STAR,
+    UNDERLINE,
+    DASH,
+    SEPARATOR,
+    TEXT,
+    HASH,
+    UNDEFINED
+}
