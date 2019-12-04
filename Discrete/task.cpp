af3b1f58d924181684d7aba6f59b73f0e5e3d262
iliayar <iliayar3@gmail.com>
2019-12-04 16:55:48 +0300

    Discrete la

--- Discrete/task.cpp
+++ Discrete/task.cpp
@@ -1,83 +1,49 @@
 #include <iostream>
-//#include <bits/stdc++.h>
+#include <string>
+#include <algorithm>
 #include <vector>
-#include <climits>
-#define int long long
-#ifndef LOCAL
-#define filename "huffman"
-#else
-#define filename "test"
-#endif
+#include <cmath>
 
-#define INF LLONG_MAX
+#define int long long
+
+#define FILENAME "local"
+
+#ifndef LOCAL
+#define FILENAME "brackets"
+#endif
 
 using namespace std;
 
-struct Node {
-    int parent;
-    int n;
-    int left;
-    int right;
-};
+int n;
 
-unsigned int res = 0;
-vector<Node> g;
+string s;
 
-void cnt(int i, int h) {
-    if(g[i].left == -1 || g[i].right == -1) {
-        res += h*g[i].n;
-//        cout <<  "Leaf: " << h << " " << i + 1 << " " << g[i].n << endl;
+void foo(int k, int len) {
+    if(len == n*2) {
+        cout << s << endl;
         return;
     }
-//    cout << h << " " << i + 1 << " " << g[i].n << endl;
-    cnt(g[i].left,h + 1);
-    cnt(g[i].right,h + 1);
+    if(n*2 - k- len >= 0) {
+        s[len] = '('; foo(k + 1, len + 1);
+    }
+    if(k > 0) {
+        s[len] = ')'; foo(k - 1, len + 1);
+    }
+
 }
 
 signed main() {
-    freopen(filename".in", "r", stdin);
-    freopen(filename".out", "w", stdout);
-    int n; cin >> n;
-    g.resize(2*n);
-    for(int i = 0; i < n; ++i) {
-        int t; cin >> t;
-        g[i].n = t;
-    }
-    for(int i = 0; i < 2*n; ++i) {
-        g[i].parent = -1;
-        g[i].left = -1;
-        g[i].right = -1;
-        if(i >= n) {
-            g[i].n = INF;
-        }
-    }
-    for(int i = n; i < 2*n; ++i) {
-        int imin1, imin2;
-        int min = INF;
-        for(int j = 0; j < 2*n; ++j) {
-            if(g[j].parent != -1) continue;
-            if(min > g[j].n) {
-                min = g[j].n;
-                imin1 = j;
-            }
-        }
-        min = INF;
-        for(int j = 0; j < 2*n; ++j) {
-            if(j == imin1 || g[j].parent != -1) continue;
-            if(min > g[j].n) {
-                min = g[j].n;
-                imin2 = j;
-            }
-        }
-        g[i].left = imin1;
-        g[i].right = imin2;
-        g[i].n = g[imin1].n + g[imin2].n;
-        g[imin1].parent = i;
-        g[imin2].parent = i;
-    }
-    cnt(2*n - 2, 0);
-    cout << res << endl;
-    fclose(stdout);
-    fclose(stdin);
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    
+    freopen(FILENAME".in", "r", stdin);
+    freopen(FILENAME".out", "w", stdout);
+
+    cin >> n;
+    cout << "TEST" << endl;
+    for(int i = 0; i < 2*n; ++i) s += ' ';
+
+    foo(0, 0);
+
     return 0;
 }
