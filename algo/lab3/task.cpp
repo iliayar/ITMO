0427d2a3bdca71d78146b5dee1447e9895de68de
iliayar <iliayar3@gmail.com>
2019-12-16 22:36:14 +0300

    Some labs

--- algo/lab3/task.cpp
+++ algo/lab3/task.cpp
@@ -3,49 +3,26 @@
 #include <algorithm>
 #include <vector>
 #include <cmath>
-
-
+ 
+ 
 using namespace std;
-
-
+ 
+ 
 #define int long long
 #define vint vector<int>
-#define INF 1 << 4
 #define ALL(a) a.begin(), a.end()
+ 
 
+const int INF = 1 << 30;
 
-int f(char c) {
-    switch (c)
-    {
-        case ')':
-        case '(':
-            return 0;
-        case '[':
-        case ']':
-            return 1;
-        case '{':
-        case '}':
-            return 2;
-    }
-}
-
-int g(char c) {
-    switch(c) {
-        case '(':
-        case '[':
-        case '{':
-            return 1;
-        case ')':
-        case ']':
-        case '}':
-            return -1;
-    }
-}
-
+vector<vint> dp;
+vector<vint> a;
+vector<vint> d;
+ 
 signed main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0); cout.tie(0);
-
+ 
 #ifdef LOCAL    
     freopen("local.in", "r", stdin);
     freopen("local.out", "w", stdout);
@@ -53,44 +30,83 @@ signed main() {
 //    freopen("input.txt", "r", stdin);
 //    freopen("output.txt", "w", stdout);
 #endif
+ 
+    int n; cin >> n;
+    
+    const int N = pow(2,n);
+ 
+    a.resize(n, vint(n));
+    dp.resize(N, vint(n, INF));
+    for(int i = 0; i < n; ++i)
+        for(int j = 0; j < n; ++j)
+            cin >> a[i][j];
+ 
+    // for(int i = 0; i < n; ++i) dp[0][i] = 0;
+    for(int i = 0; i < n; ++i) dp[1 << i][i] = 0;
+    // for(int i = 0; i < n; ++i) 
+    //     for(int j = 0; j < n; ++j){
+    //         dp[(1 << j) | (1 << i)][i] = a[i][j];
+    //         d[(1 << j) | (1 << i)][i] = j;
+    //     }
+    for(int mask = 0; mask < N; ++mask) {
+        for(int i = 0; i < n; ++i) {
+            if(!((mask >> i) & 1)) continue;
+            for(int j = 0; j < n; ++j) {
+                if(!((mask >> j) & 1)) continue;
+                dp[mask][i] = min(dp[mask][i], dp[mask ^ (1 << i)][j] + a[i][j]);
+            }
+        }
+    }
+    
+ 
+ 
+    // for(int i = 0; i < N; ++i) {
+    //     for(int j = 0; j < n; ++j) {
+    //         if(dp[i][j] == INF) {
+    //             printf("%5s", "-");
+    //             continue;
+    //         }
+    //         printf("%5Ld", dp[i][j]);
+    //     }
+    //     printf("\n");
+    // }
+    // printf("_____________________________________\n");
+ 
+    // for(int i = 0; i < N; ++i) {
+    //     for(int j = 0; j < n; ++j) {
+    //         if(d[i][j] == INF) {
+    //             printf("%5s", "-");
+    //             continue;
+    //         }
+    //         printf("%5Ld", d[i][j]);
+    //     }
+    //     printf("\n");
+    // }
+ 
+    int id = -1;
+    int m = INF;
 
-    string s; cin >> s;
-    int n = s.length();
-    vector<vector<int>> dp[3];
-    
-    dp[0].resize(n + 1, vint(n + 1 , INF));
-    dp[1].resize(n + 1, vint(n + 1 , INF));
-    dp[2].resize(n + 1, vint(n + 1 , INF));
-    
-    // for(int i = 0; i < n + 1; ++i) dp[0][0][i] = dp[1][0][i] = dp[2][0][i] = 0;
-    
-    dp[0][0][0] = dp[1][0][0] = dp[2][0][0] = 0;
 
     for(int i = 0; i < n; ++i) {
-        for(int j = 0; j < n + 1; ++j) {
-            dp[f(s[i])][i + 1][j] = min(dp[f(s[i])][i + 1][j], dp[f(s[i])][i][j]) + 1;
-
-
-            for(int k = 0; k < 3; ++k) {
-                if(k == f(s[i]) && j + g(s[i]) >= 0 && j + g(s[i]) < n + 1) 
-                    dp[k][i + 1][j + g(s[i])] = min(dp[k][i + 1][j + g(s[i])] , dp[k][i][j]);
-                else if(k != f(s[i]))
-                    dp[k][i + 1][j] = min(dp[k][i + 1][j] , dp[k][i][j]);
+        if(dp[N-1][i] < m) {
+            m = dp[N - 1][i];
+            id = i;
+        }
+    }
+    cout << m << endl;
+    int mask = N - 1;
+    // cout << id << endl;
+    for(int i = 0; i < n; ++i) {
+        cout << id + 1<< " ";
+        for(int j = 0; j < n; ++j) {
+            if(dp[mask][id] == dp[mask ^ (1 << id)][j] + a[id][j]) {
+                mask = mask ^ (1 << id);
+                id = j;
+                break;
             }
-            
-            // for(int k = 0; k < 3; ++k)
-
         }
     }
-    
-    for(int i = 0; i < n + 1; ++i) {
-        for(int j = 0; j < n + 1; ++j) {
-            printf("(%2Ld, %2Ld, %2Ld) ", dp[0][i][j],dp[1][i][j],dp[2][i][j]);
-        }
-        printf("\n");
-    }
-
-
+    cout << endl;
 
     return 0;
 } 
