04a9d3cce2d27c4caed94d23e1a978e5409080c4
iliayar <iliayar3@gmail.com>
2019-12-19 16:51:02 +0300

    HW11 parser shifts reversedigits

---
+++ programming/HW11/src/expression/ShiftLeft.java
@@ -0,0 +1,29 @@
+package expression;
+
+public class ShiftLeft extends Operation {
+
+
+    public ShiftLeft(Expression a, Expression b) {
+        super(a, b);
+    }
+
+    @Override
+    protected String getSymbol() {
+        return "<<";
+    }
+
+    @Override
+    protected int eval(int a, int b) {
+        return a << b;
+    }
+
+    @Override
+    public int getPrior() {
+        return 3;
+    }
+
+    @Override
+    public boolean isCommutative() {
+        return false;
+    }
+}
