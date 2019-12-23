e77a40e2ee1cc67e17b2b957fb0de0d8b484888e
iliayar <iliayar3@gmail.com>
2019-12-23 21:49:16 +0300

    Discrete lab3 local

--- Discrete/LAB3/task.cpp
+++ Discrete/LAB3/task.cpp
@@ -6,24 +6,78 @@
 
 #define int long long
 #define vint vector<int>
+#define ALL(a) a.begin(), a.end()
 
 #define FILENAME "local"
-
+ 
 #ifndef LOCAL
-#define FILENAME "num2brackets"
+#define FILENAME "nextperm"
 #endif
-
+ 
 using namespace std;
 
 
 signed main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0); cout.tie(0);
-    
+     
     freopen(FILENAME".in", "r", stdin);
     freopen(FILENAME".out", "w", stdout);
 
-    int n, k; cin >> n >> k;
+    int n; cin >> n;
 
+    vint a(n);
+    for(int i = 0; i < n; ++i) cin >> a[i];
+
+    vint t(0);
+    
+    //PREV
+    int i = a.size() - 1;
+    while(i > 0 && a[i-1] < a[i]) {
+        t.push_back(a[i]);
+        i--;
+    }
+    if(i == 0) {
+        for(int j = 0; j < n; ++j) cout << 0 << ' ';
+    } else {
+        t.push_back(a[i]);
+        t.push_back(a[i-1]);
+        int p = a[i-1];
+        for(int j = 0; j < i - 1; ++j) {
+            cout << a[j] << ' ';
+        }
+        sort(ALL(t));
+        i = 0;
+        while(t[++i] < p);
+        int tp = t[i-1]; cout << tp << ' ';
+        for(int j = t.size() - 1; j >= 0; --j) {
+            if(t[j] != tp) cout << t[j] << ' ';
+        }
+    }
+    cout << endl;
+    //NEXT
+    t.clear();
+    i = a.size() - 1;
+    while(i > 0 && a[i-1] > a[i]) {
+        t.push_back(a[i]);
+        i--;
+    }
+    if(i == 0) {
+        for(int j = 0; j < n; ++j) cout << 0 << ' ';
+    } else {
+        t.push_back(a[i]);
+        t.push_back(a[i-1]);
+        int p = a[i-1];
+        for(int j = 0; j < i - 1; ++j) {
+            cout << a[j] << ' ';
+        }
+        sort(ALL(t));
+        i = t.size();
+        while(t[--i] > p);
+        int tp = t[i+1]; cout << tp << ' ';
+        for(int j = 0; j < t.size(); ++j) {
+            if(t[j] != tp) cout << t[j] << ' ';
+        }
+    }
     return 0;
 }
