4473bb4494dd3cdbf02c7bab429f3c707aafbf4e
iliayar <iliayar3@gmail.com>
2020-02-21 13:15:58 +0300

    Some fixes

--- Sem1/programming/HW11/src/expression/exceptions/CheckedMultiply.java
+++ Sem1/programming/HW11/src/expression/exceptions/CheckedMultiply.java
@@ -9,14 +9,15 @@ public class CheckedMultiply extends Multiply {
         super(a, b);
     }
 
-
+    public static void checkOverflow(int a, int b, String msg) {
+        if ((a > 0 && ((b > 0 && b > Integer.MAX_VALUE / a) || (b < 0 && b < Integer.MIN_VALUE / a))) ||
+                (a < 0 && ((b > 0 && a < Integer.MIN_VALUE / b) || (b < 0 && b < Integer.MAX_VALUE / a)))) {
+            throw new IntegerOverflowException(msg);
+        }
+    }
     @Override
     public int eval(int a, int b) {
-        if((a > 0 && ((b > 0 && b > Integer.MAX_VALUE/a) || (b < 0 && b < Integer.MIN_VALUE/a))) ||
-                (a < 0 && ((b > 0 && a < Integer.MIN_VALUE/b) || (b < 0 && b < Integer.MAX_VALUE/a)))) {
-
-            throw new IntegerOverflowException(a+"*"+b);
-        }
+        checkOverflow(a,b, a + "*" + b);
 
         return a*b;
     }
--- Sem1/programming/HW11/src/expression/exceptions/Pow.java
+++ Sem1/programming/HW11/src/expression/exceptions/Pow.java
@@ -29,19 +29,23 @@ public class Pow extends Operation {
     }
 
     public int eval(int a, int b) {
-
-        if(b < 0 || (a == 0 && b == 0)) {
-            throw new PowException(a,b);
+        int at = a, bt = b;
+        if (b < 0 || (a == 0 && b == 0)) {
+            throw new PowException(a, b);
         }
 
         int res = 1;
 
-        for(int i = 0; i < b; ++i) {
-            if((res > 0 && ((a > 0 && a > Integer.MAX_VALUE/res) || (a < 0 && a < Integer.MIN_VALUE/res))) ||
-                    (res < 0 && ((a > 0 && res < Integer.MIN_VALUE/a) || (a < 0 && a < Integer.MAX_VALUE/res)))) {
-                throw new IntegerOverflowException(a+"**"+b);
+        while (b > 0) {
+            if (b % 2 == 1){
+                CheckedMultiply.checkOverflow(res,a, at + "**" + bt);
+                res *= a;
+                b -= 1;
+            } else {
+                CheckedMultiply.checkOverflow(a, a, at + "**" + bt);
+                a *= a;
+                b /= 2;
             }
-            res *= a;
         }
 
         return res;
