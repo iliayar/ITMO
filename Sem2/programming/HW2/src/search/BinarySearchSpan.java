1940b575ae32c0c654b159b8a5091c9065aff03e
iliayar <iliayar3@gmail.com>
2020-02-26 03:31:39 +0300

    Proofs HW2 prog

--- Sem2/programming/HW2/src/search/BinarySearchSpan.java
+++ Sem2/programming/HW2/src/search/BinarySearchSpan.java
@@ -24,6 +24,8 @@ public class BinarySearchSpan {
         int l = upperBound(x, a);
         int r = lowerBound(x, a);
 
+        // Pre: not exist v in a: v = x
+        // Post: insertion position
         if(l >= a.length) {
             r = l - 1;
         } else if(a[l] != x) {
@@ -37,18 +39,41 @@ public class BinarySearchSpan {
 
 
 
+    // Pre: I = [0 : a.length) and  forall j,k in I: j > k a[j] <= a[k]
+    // Post: i: i = max({j in I: a[j] <= x})
     private int lowerBound(int x, int[] a) {
+
+        // Pre: min(a) > x
+        // Post: insertion position a.length
         if (a[a.length - 1] > x) {
             return a.length;
         }
 
+        // Pre: a.length > 0
+        // Post: l = 0 and r = max(I) + 1 and [l : r) = I
         int l = 0, r = a.length;
 
+        // let a[r] = -INF
+        // inv: [l' : r') subset I and a[r'] < x <= a[l'] and |[l' : r')| > 1 and |[l' : r')| < |[l : r)|
+        // Pre: l = 0 and r = max(I) + 1 and [l : r) = I
+        // Post: |[l' : r')| = 1 and l = max({j in I: a[j] >= x}) Д-во от обратного a[l+1 = r] >= x
         while (r - l > 1) {
+
+            // Pre: |[l' : r')| > 1
+            // Post l < m < r
             int m = (l + r)/2;
+
+            // Pre: l < m < r and a[r] <= x <= a[l]
+            // Post: |[l' : r')| < |[l : r)| and  a[r'] <= x <= a[l']
             if (x <= a[m]) {
+
+                // Pre: l < m < r and x <= a[m]
+                // Post: |[l' : r)| < |[l : r)| and a[l'] >= x
                 l = m;
             } else {
+
+                // Pre: l < m < r in [l : r) and x > a[m]
+                // Post: |[l : r')| < |[l : r)| and a[r'] < x
                 r = m;
             }
         }
@@ -58,7 +83,13 @@ public class BinarySearchSpan {
 
 
 
+
+    // Pre: I = [0 : a.length) and forall j,k in I: j > k a[j] <= a[k]
+    // Post: i: i = min({j in I: a[j] <= x})
     private int upperBound(int x, int [] a) {
+
+        // Pre: min(a) > x
+        // Post: insertion position a.length
         if (a[a.length - 1] > x) {
             return a.length;
         }
