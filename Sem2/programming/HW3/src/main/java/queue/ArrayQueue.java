3b468a24f9db083e8c6a3a5cd4f576168cce0241
iliayar <iliayar3@gmail.com>
2020-02-26 18:18:42 +0300

    prog HW3

---
+++ Sem2/programming/HW3/src/main/java/queue/ArrayQueue.java
@@ -0,0 +1,62 @@
+package queue;
+
+
+
+public class ArrayQueue {
+
+    private int tail = 0;
+    private int head = 0;
+
+    private int length = 0;
+
+    private Object[] array = new Object[2];
+
+    private void expandArrayNeeded() {
+        if(this.array[this.tail] == null) {
+            return;
+        }
+        Object[] temp = new Object[this.array.length*2];
+        System.arraycopy(this.array, this.head, temp, this.array.length + this.head, this.array.length - this.head);
+        System.arraycopy(this.array, 0, temp, 0, this.tail);
+        this.head = this.array.length + this.head;
+        this.array = temp;
+    }
+
+    public void enqueue(Object x) {
+        expandArrayNeeded();
+        this.array[this.tail] = x;
+        this.tail = (this.tail+1) % this.array.length;
+        this.length++;
+        // printArray();
+    }
+
+    public Object dequeue() {
+        if(this.array[this.head] == null) {
+            return null;
+        }
+        Object t = this.array[this.head];
+        this.array[this.head] = null;
+        this.head = (this.head + 1) % this.array.length;
+        this.length--;
+        // printArray();
+        return t;
+    }
+
+    public Object element() {
+        return this.array[this.head];
+    }
+
+    public int size() {
+        return this.length;
+    }
+
+    public boolean isEmpty() {
+        return this.length == 0;
+    }
+    public void clear() {
+        this.array = new Object[2];
+        this.length = 0;
+        this.head = 0;
+        this.tail = 0;
+    }
+}
