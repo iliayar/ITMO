6f226659de1cc53d8f9cfea671dcf99eb17417c3
Alexander Kirakozov <akirakozov@yandex-team.ru>
2020-03-15 16:57:35 +0300

    add test-containers example

---
+++ java/test-containers-example/src/test/java/ru/akirakozov/sd/app/AppTest.java
@@ -0,0 +1,33 @@
+package ru.akirakozov.sd.app;
+
+import org.junit.Assert;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.testcontainers.containers.FixedHostPortGenericContainer;
+import org.testcontainers.containers.GenericContainer;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class AppTest {
+
+    @ClassRule
+    public static GenericContainer simpleWebServer
+            = new FixedHostPortGenericContainer("test-containers-example:1.0-SNAPSHOT")
+            .withFixedExposedPort(8080, 8080)
+            .withExposedPorts(8080);
+
+    @Test
+    public void test() throws Exception {
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(new URI("http://localhost:8080/hello?name=Petr"))
+                .GET()
+                .build();
+
+        HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+        Assert.assertEquals("Hello, Petr!", response.body());
+    }
+
+}
