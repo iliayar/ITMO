00629bb4bed8ced122ecc57aab3761bc0fc2421a
iliayar <iliayar3@gmail.com>
2020-03-18 02:28:24 +0300

    Prog uncheked fixes

--- Sem2/programming/HW5/src/main/java/expression/generic/GenericTabulator.java
+++ Sem2/programming/HW5/src/main/java/expression/generic/GenericTabulator.java
@@ -10,41 +10,34 @@ public class GenericTabulator implements Tabulator {
 	@Override
 	public Object[][][] tabulate(String mode, String expression, int x1, int x2, int y1, int y2, int z1, int z2)
 			throws Exception {
-        Object[][][] table = new Object[x2 - x1 + 1][y2 - y1 + 1][z2 - z1 + 1];
 
 //        TripleExpression e;
 //        Calculator calc;
         if(mode.equals( "i")) {
-            Calculator<Integer> calc = new IntegerCalculator();
-            TripleExpression<Integer> e = new ExpressionParser<Integer>(calc).parse(expression);
+            return getTable(new ExpressionParser<Integer>(new IntegerCalculator()), expression, x1, y1, z1, x2, y2, z2);
         }
         else if(mode.equals("d")) {
-            Calculator<Double> calc = new DoubleCalculator();
-            TripleExpression<Double> e = new ExpressionParser<Double>(calc).parse(expression);
+            return getTable(new ExpressionParser<Double>(new DoubleCalculator()), expression, x1, y1, z1, x2, y2, z2);
         }
         else if(mode.equals("bi")){
-            Calculator<BigInteger> calc = new BigIntegerCalculator();
-            TripleExpression<BigInteger> e = new ExpressionParser<BigInteger>(calc).parse(expression);
+            return getTable(new ExpressionParser<BigInteger>(new BigIntegerCalculator()), expression, x1, y1, z1, x2, y2, z2);
         }
         else if(mode.equals("l")) {
-            Calculator<Long> calc = new LongCalculator(false);
-            TripleExpression<Long> e = new ExpressionParser<Long>(calc).parse(expression);
+            return getTable(new ExpressionParser<Long>(new LongCalculator(false)), expression, x1, y1, z1, x2, y2, z2);
          }
-        else if(mode.equals("bi")) {
-            Calculator<Short> calc = new ShortCalculator(false);
-            TripleExpression<Short> e = new ExpressionParser<Short>(calc).parse(expression);
+        else if(mode.equals("s")) {
+            return getTable(new ExpressionParser<Short>(new ShortCalculator(false)), expression, x1, y1, z1, x2, y2, z2);
         }
         else if(mode.equals("u")) {
-            getTable(new ExpressionParser<Integer>(new IntegerCalculator(false)), expression, table, x1, y1, z1);
+            return getTable(new ExpressionParser<Integer>(new IntegerCalculator(false)), expression, x1, y1, z1, x2, y2, z2);
         } else {
             throw new Exception("Unknown number format");
         }
-
-		return table;
 	}
 
-    private <T extends Number> void getTable(ExpressionParser<T> parser, String expression, Object[][][] table, int x1, int y1, int z1) {
+    private <T extends Number> Object[][][] getTable(ExpressionParser<T> parser, String expression, int x1, int y1, int z1, int x2, int y2, int z2) {
         TripleExpression<T> e = parser.parse(expression);
+        Object[][][] table = new Object[x2 - x1 + 1][y2 - y1 + 1][z2 - z1 + 1];
         for (int x = 0; x < table.length; ++x) {
             for(int y = 0; y < table[x].length; ++y) {
                 for(int z = 0; z < table[x][y].length; ++z) {
@@ -52,5 +45,6 @@ public class GenericTabulator implements Tabulator {
                 }
             }
         }
+        return table;
     }
 }
