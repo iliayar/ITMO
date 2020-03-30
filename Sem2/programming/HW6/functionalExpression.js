31361299f721c81579c5c6cbd96af8aa0833be10
iliayar <iliayar3@gmail.com>
2020-03-31 00:25:07 +0300

    Prog HW6 fixes

--- Sem2/programming/HW6/functionalExpression.js
+++ Sem2/programming/HW6/functionalExpression.js
@@ -1,38 +1,76 @@
 "use strict";
 
-
-let cnst = value => () => value;
-let variable = variable => (x, y, z) =>  {
-    if(variable == "x") {
-        return x;
-    } else if(variable == "y") {
-        return y;
-    } else {
-        return z;
-    }
+let variables = {
+    "x": 0,
+    "y": 1,
+    "z": 2
 };
 
-let binOperation = op => (a,b) => (x, y, z) => op(a(x, y, z),b(x, y, z));
-let multiply = binOperation((a,b) => a*b);
-let add = binOperation((a,b) => a+b);
-let subtract = binOperation((a,b) => a-b);
-let divide = binOperation((a,b) => a/b);
+let cnst = value => () => value;
+let expressionVariable = variable => (...args) => args[variables[variable]];
+
+let cachedVariables  = {};
+Object.keys(variables).forEach(v => cachedVariables[v] = expressionVariable(v));
+let variable = v => cachedVariables[v];
+
+let binOperation = op => (a, b) => (...args) => op(a(...args), b(...args));
+let multiply = binOperation((a, b) => a * b);
+let add = binOperation((a, b) => a + b);
+let subtract = binOperation((a, b) => a - b);
+let divide = binOperation((a, b) => a / b);
 
 let pi = cnst(Math.PI);
 let e = cnst(Math.E);
 
-let avg5 = function() {
-    return (x, y, z) => Array.from(arguments).slice(0, 5).reduce((acc, element) => acc + element(x,y,z), 0)/5;
-};
+let avg5 = (...values) =>
+    (...args) => values.reduce((acc, value) => acc + value(...args), 0)/values.length;
 
-let med3 = function() {
-    return (x,y,z) => Array.from(arguments).slice(0,3).sort((a, b) => a(x,y,z) - b(x, y, z))[1](x, y, z);
-}
+let med3 = (...values) =>
+    (...args) => values.map(x => x(...args)).sort((a, b) => a - b)[Math.floor(values.length/2)];
 
-let unaryOpeartion = op => a => (x, y, z) => op(a(x, y, z));
+
+let unaryOpeartion = op => a => (...args) => op(a(...args));
 let negate = unaryOpeartion(a => -a);
 
-// TESTS
+// Parser
 
-console.log(avg5(cnst(5), cnst(3), variable("x"), variable("z"), cnst(-5))(3, 0, -3));
-// console.log(med3(variable("y"), variable("x"), variable("z"))(0, 0, 1));
+let tokens = {}
+let addOperation = (token, arity, func) => tokens[token] = {"arity": arity, "func": func};
+let addVariables = () => Object.keys(variables).forEach(v => addOperation(v, 0, () => variable(v)));
+addOperation("negate", 1, negate);
+addOperation("+",      2, add);
+addOperation("-",      2, subtract);
+addOperation("/",      2, divide);
+addOperation("*",      2, multiply);
+addOperation("med3",   3, med3);
+addOperation("avg5",   5, avg5);
+addOperation("pi",     0, () => pi);
+addOperation("e",      0, () => e);
+addVariables();
+
+let parseToken = function(token, stack) {
+   if(token in tokens) {
+       var oper = tokens[token];
+       // console.log(oper);
+       return oper.func(...stack.splice(stack.length - oper.arity));
+   } else if(!isNaN(token)) {
+       return cnst(+token);
+   } else {
+       throw new Error("Unkown token: " + token);
+   }
+}
+
+let parseArray = function(arr) {
+    var stack = [];
+    for(const token of arr) {
+        if(token.length == 0) continue;
+        stack.push(parseToken(token, stack));
+        // console.log(stack);
+    }
+    return stack[0];
+}
+
+let parse = expressionString => parseArray(expressionString.split(' '));
+
+// console.log(tokens);
+console.log(parse("x x *")(2,0,0));
