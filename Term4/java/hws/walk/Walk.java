cf2394faeb14a8249f7c8c2bbb21a987fe396923
Ilya <iliayar3@gmail.com>
2021-03-01 15:46:00 +0300

    Java HW1

---
+++ Term4/java/hws/walk/Walk.java
@@ -0,0 +1,15 @@
+package info.kgeorgiy.ja.yaroshevskij.walk;
+
+import java.nio.file.*;
+
+public class Walk {
+
+    public static void main(String[] args) {
+
+	    for(String entry : args) {
+		    // :TODO: Something with entry
+	    }
+
+    }
+
+}
