ed7e21bd1ff7b78d7a0f00c66ef35200f9c7d9d2
Ilya <iliayar3@gmail.com>
2021-03-25 00:48:33 +0300

    Some dm rust labs

--- other/lab/src/main.rs
+++ other/lab/src/main.rs
@@ -1,6 +1,6 @@
 use std::fs::File;
 use std::io::{self, Read};
-
+use std::str::{FromStr};
 
 struct Scanner<'a> {
     pos: usize,
@@ -31,7 +31,7 @@ impl<'a> Scanner<'a> {
 	}
     }
 
-    fn read_int(&mut self) -> Option<u64> {
+    fn read_num<T: FromStr>(&mut self) -> Option<T> {
 	self.skip_whitespaces();
 	let mut s = String::new();
 	while let Some(c) = self.test_digit() {
@@ -40,10 +40,10 @@ impl<'a> Scanner<'a> {
 	return s.parse().ok()
     }
 
-    fn read_vec(&mut self, count: usize) -> Result<Vec<u64>, Vec<u64>> {
+    fn read_vec<T: FromStr>(&mut self, count: usize) -> Result<Vec<T>, Vec<T>> {
 	let mut vec = Vec::new();
-	for i in 0..count {
-	    if let Some(n) = self.read_int() {
+	for _ in 0..count {
+	    if let Some(n) = self.read_num() {
 		vec.push(n);
 	    } else {
 		return Err(vec);
@@ -73,10 +73,15 @@ impl<'a> Scanner<'a> {
     fn check_size(&mut self) {
 	if self.pos >= self.len {
 	    if let Ok(len) = self.fill() {
-		self.len = len;
-		self.pos = 0;
+		if len == 0 {
+		    self.eof = true;
+		} else {
+		    self.len = len;
+		    self.pos = 0;
+		}
 	    } else {
 		self.eof = true;
+		eprintln!("Scanner failed to read bytes");
 	    }
 	}
     }
@@ -108,23 +113,51 @@ impl<'a> Scanner<'a> {
     }
 
     fn skip_whitespaces(&mut self) {
-	while let Some(_) = self.test_chars(&vec![b'\n', b' ', b'\t']) {
+	while let Some(_) = self.test_chars(&vec![b'\n', b' ', b'\t']) { }
+    }
+}
+
+
+// CODE_HERE
+
+fn sum(g1: &Vec<u64> , g2: &Vec<u64>) -> Vec<u64> {
+    let mut p = Vec::new();
+    p.resize(std::cmp::max(g1.len(), g2.len()), 0);
+    for i in 0..p.len() {
+	p[i] = if i >= g1.len() {
+	    0
+	} else {
+	    g1[i]
+	} + if i >= g2.len() {
+	    0
+	} else {
+	    g2[i]
+	};
+    }
+    return p
+}
+
+fn mult(g1: &Vec<u64>, g2: &Vec<u64>) -> Vec<u64> {
+    let mut p = Vec::new();
+    p.resize(g1.len() + g2.len(), 0);
+    for i in 0..g1.len() {
+	for j in 0..g2.len() {
+	    p[i + j] += g1[i] * g2[j];
 	}
     }
-
-
+    p
 }
 
 fn sol(scanner: &mut Scanner) {
-
-    let n = scanner.read_int().unwrap();
-    let m = scanner.read_int().unwrap();
-    let a = scanner.read_vec(n as usize).unwrap();
-    println!("{:?}", a);
+    let n: u64 = scanner.read_num().unwrap();
+    let _m: u64 = scanner.read_num().unwrap();
+    let p1: Vec<u64> =  scanner.read_vec((n + 1) as usize).unwrap();
+    let p2: Vec<u64> = scanner.read_vec((n + 1) as usize).unwrap();
+    let p = mult(&p1, &p2);
+    println!("{:?}", p);
 }
 
 fn main() {
-    let local = false;
     let mut stdin;
     let mut file;
 
