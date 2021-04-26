ac90a8c719bf08697ec49067f96dbfc73d32ed39
Ilya <iliayar3@gmail.com>
2021-04-26 14:54:52 +0300

    Fix some issues in 8 Homework

---
+++ Term4/java/hws/java-solutions/module-info.java
@@ -0,0 +1,9 @@
+module info.kgeorgiy.ja.yaroshevskij.implementor {
+    exports info.kgeorgiy.ja.yaroshevskij.implementor;
+    exports info.kgeorgiy.ja.yaroshevskij.concurrent;
+    requires info.kgeorgiy.java.advanced.implementor;
+    requires info.kgeorgiy.java.advanced.mapper;
+    requires info.kgeorgiy.java.advanced.concurrent;
+    requires java.compiler;
+    opens info.kgeorgiy.ja.yaroshevskij.implementor;
+}
