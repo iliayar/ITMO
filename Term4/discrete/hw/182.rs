e7dac3854cbb73e78ac77383c50691b3c2347766
Ilya <iliayar3@gmail.com>
2021-05-12 13:27:07 +0300

    183, 184

--- Term4/discrete/hw/182.rs
+++
@@ -1,6 +0,0 @@
-fn main() {
-    let s = "fn main() {\n    let s = ?;\n    let s = str::replacen(s, \"?\", &format!(\"{:?}\", s), 1);\n    println!(\"{}\", s);\n}\n";
-    let s = str::replacen(s, "?", &format!("{:?}", s), 1);
-    println!("{}", s);
-}
-
