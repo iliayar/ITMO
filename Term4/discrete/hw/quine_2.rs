0ae4b6e970e85ab15d7dee2b6b04ca6e312d1e11
Ilya <iliayar3@gmail.com>
2021-05-12 13:27:07 +0300

    183, 184

--- Term4/discrete/hw/quine_2.rs
+++
@@ -1,4 +0,0 @@
-fn main() {
-    let (s1, s2) = ("fn main() {\n    let (s1, s2) = (", ");\n    println!(\"{}{}{}\", s1, &format!(\"{:?}, {:?}\", s1, s2), s2);\n}\n");
-    println!("{}{}{}", s1, &format!("{:?}, {:?}", s1, s2), s2);
-}
