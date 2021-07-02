ca4372f62e50409b7aad86f6323945001fa62ed7
Ilya <iliayar3@gmail.com>
2021-07-03 02:10:03 +0300

    ✔ Matlog finished
    + Speed up B Task
    + Fix C Task

--- Term4/matlog/labs/B/Converter.hs
+++ Term4/matlog/labs/B/Converter.hs
@@ -2,19 +2,24 @@ module Converter where
 
 import qualified ProofChecker as PC
 import qualified Proof as P
+import qualified Data.Map as M
 import Natural
 
 import Data.List (last)
 
 convert :: [P.Expression] -> P.Expression -> [(PC.Reason, P.Expression)] -> Node
-convert = convert' [] []
+convert = convert' M.empty M.empty
 
-convert' :: [(Statement -> Node)] -> [P.Expression] -> [P.Expression] -> P.Expression -> [(PC.Reason, P.Expression)] -> Node
+convert' :: M.Map Int (Statement -> Node) -> M.Map Int P.Expression -> [P.Expression] -> P.Expression -> [(PC.Reason, P.Expression)] -> Node
 convert' nodes es hypos st ((r, e):ls) = convert'
-    (nodes ++ 
-    [ case r of
+    (M.insert (M.size nodes)
+    (case r of
         (PC.Axiom i) -> axioms !! i
-        (PC.ModusPonens i j) -> makeImplDel (convertExpression $ es !! i) (nodes !! j) (nodes !! i)
-        PC.Hypo -> makeAxiom ]) (es ++ [e]) hypos st ls
-convert' nodes es hypos st [] = last nodes (map convertExpression hypos, convertExpression st)
+        (PC.ModusPonens i j) ->
+          let Just ei = M.lookup i es
+              Just nj = M.lookup j nodes
+              Just ni = M.lookup i nodes
+          in makeImplDel (convertExpression $ ei) nj ni
+        PC.Hypo -> makeAxiom) nodes) (M.insert (M.size es) e es) hypos st ls
+convert' nodes es hypos st [] = (snd $ M.findMax nodes) (map convertExpression hypos, convertExpression st)
 
