448e3b07a964cb9f40f8c149399be9235128d56e
iliayar <iliayar3@gmail.com>
2021-12-14 04:11:59 +0300

    da: lab6: started

--- Term5/da/flake.nix
+++ Term5/da/flake.nix
@@ -3,34 +3,35 @@
   
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.05";
-    # nixpkgs.url = "nixpkgs/a5d03577f0161c8a6e713b928ca44d9b3feb2c37";
-    jupyterlab_vim_src = {
-      url = "github:jwkvam/jupyterlab-vim";
-      flake = false;
-    };
+    # nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    # jupyterlab_vim_src = {
+      # url = "github:jwkvam/jupyterlab-vim";
+      # flake = false;
+    # };
   };
 
-  outputs = { self, nixpkgs, flake-utils, jupyterlab_vim_src }:
+  outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem
       (system:
         let
-          pkgs = import nixpkgs { inherit system; };
-          jupyterlab_vim = pkgs.stdenv.mkDerivation {
-            name = "jupyterlab_vim";
-            src = jupyterlab_vim_src;
-            nativeBuildInputs = [
-              pkgs.python39Packages.jupyter-packaging
-            ];
-            installPhase = ''
-              mkdir -p $out/share/jupyter/lab/extensions
-              cp -r $src $out/share/jupyter/lab/extensions/jupyterlab_vim
-            '';
-          };
+          pkgs = import nixpkgs { inherit system; config = { allowUnfree = true; }; };
+          # jupyterlab_vim = pkgs.stdenv.mkDerivation {
+            # name = "jupyterlab_vim";
+            # src = jupyterlab_vim_src;
+            # nativeBuildInputs = [
+              # pkgs.python39Packages.jupyter-packaging
+            # ];
+            # installPhase = ''
+              # mkdir -p $out/share/jupyter/lab/extensions
+              # cp -r $src $out/share/jupyter/lab/extensions/jupyterlab_vim
+            # '';
+          # };
           pythonEnv = pkgs.python38.withPackages (pypkgs: with pypkgs; [
             statsmodels
             jupyterlab
             # jupyterlab_vim
+            ipywidgets
             numba
             umap-learn
             seaborn
@@ -39,6 +40,11 @@
             matplotlib
             numpy
             scikit-learn
+            opencv4
+            # (pytorch.override { cudaSupport = true; })
+            pytorch-bin
+            torchvision-bin
+            tensorflow-tensorboard
           ]);
         in
         {
