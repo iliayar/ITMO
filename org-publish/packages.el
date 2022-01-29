cbe10ca1b3339f488c3f03af7d0cc0ba7ccc20fb
iliayar <iliayar3@gmail.com>
2022-01-29 05:26:32 +0300

    Term5: finished ✅

--- org-publish/packages.el
+++ org-publish/packages.el
@@ -1,3 +1,15 @@
+(require 'package)
+(add-to-list
+ 'package-archives
+ '("melpa" . "https://melpa.org/packages/")
+ t)
+
+(package-initialize)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
 (add-to-list 'load-path "/publish/config/lisp/")
 
 (use-package haskell-mode
