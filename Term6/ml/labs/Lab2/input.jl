39d35edd4d5ab8a7270f2c9efe4b2d0da75ebe63
iliayar <iliayar3@gmail.com>
2022-03-18 23:03:52 +0300

    ml: Lab2: done ✔

--- Term6/ml/labs/Lab2/input.jl
+++ Term6/ml/labs/Lab2/input.jl
@@ -1,4 +1,3 @@
-include("object.jl")
 
 function read_data(filename::String)::Tuple{Vector{Object}, Vector{Object}}
     train::Vector{Object} = []
@@ -7,7 +6,6 @@ function read_data(filename::String)::Tuple{Vector{Object}, Vector{Object}}
     open("data/$filename") do f
         M = parse(Int64, readline(f))
         N = parse(Int64, readline(f))
-        # println("N = $N, M = $M")
 
         function read_part!(n, d)
             for _ in 1:n
--- Term6/ml/labs/Lab2/object.jl
+++ Term6/ml/labs/Lab2/object.jl
@@ -1,6 +1,6 @@
 
 
-struct Object
+mutable struct Object
     X::Vector{Float64}
     y::Float64
 end
