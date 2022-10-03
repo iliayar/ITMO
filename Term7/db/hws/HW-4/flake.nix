1034f77d9f686b89cf64ccb50dd578452ea29901
iliayar <iliayar3@gmail.com>
2022-10-03 21:20:20 +0300

    db: HW-4 done

---
+++ Term7/db/hws/HW-4/flake.nix
@@ -0,0 +1,53 @@
+{
+  description = "DataBase Homework 1";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-22.05";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+          };
+        in
+        {
+          devShell = pkgs.mkShell rec {
+            buildInputs = with pkgs; [
+              graphviz
+
+              postgresql_14
+
+              (writeScriptBin "start_pg" ''
+                pg_ctl start -l $LOG_PATH -o "-c listen_addresses= -c unix_socket_directories=$PGHOST"
+              '')
+
+              (writeScriptBin "stop_pg" ''
+                pg_ctl stop
+              '')
+            ];
+
+
+            shellHook = ''
+              export PGDATA="$PWD/postgres_data";
+              export PGHOST="$PWD/postgres";
+              export LOG_PATH="$PWD/postgres/LOG";
+              export PGDATABASE="postgres";
+              export DATABASE_URL="postgresql://postgres?host=$PGHOST";
+              
+              if [ ! -d $PGHOST ]; then
+                mkdir -p $PGHOST
+              fi
+
+              if [ ! -d $PGDATA ]; then
+                echo "Initializing postgresql database..."
+                initdb $PGDATA --auth=trust
+              fi
+            '';
+          };
+        }
+    );
+}
