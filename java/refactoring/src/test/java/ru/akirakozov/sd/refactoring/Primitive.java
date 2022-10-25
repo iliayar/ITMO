093e0a951cf2d32beec82e89dfa19a69a3ba00ea
iliayar <iliayar3@gmail.com>
2022-10-25 03:00:07 +0300

    Initial tests

---
+++ java/refactoring/src/test/java/ru/akirakozov/sd/refactoring/Primitive.java
@@ -0,0 +1,12 @@
+package ru.akirakozov.sd.refactoring;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class Primitive {
+
+  @Test
+  void primitive() {
+    assertTrue(true);
+  }
+}
