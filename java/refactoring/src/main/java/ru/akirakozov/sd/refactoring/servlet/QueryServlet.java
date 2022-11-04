073cb2645c8f73c8fa39454591a50dca2cbd92bb
iliayar <iliayar3@gmail.com>
2022-11-04 16:08:01 +0300

    Remove duplication from query handler

--- java/refactoring/src/main/java/ru/akirakozov/sd/refactoring/servlet/QueryServlet.java
+++ java/refactoring/src/main/java/ru/akirakozov/sd/refactoring/servlet/QueryServlet.java
@@ -1,5 +1,8 @@
 package ru.akirakozov.sd.refactoring.servlet;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
 
 import ru.akirakozov.sd.refactoring.database.Database;
@@ -11,28 +14,62 @@ import ru.akirakozov.sd.refactoring.models.Product;
  */
 public class QueryServlet extends AbstractGetHtmlServlet {
 
+  Map<String, CommandHandler> handlers;
+
   public QueryServlet(Database database) {
     super(database);
+
+    handlers = new HashMap<>();
+    handlers.put("max", new MaxHandler());
+    handlers.put("min", new MinHandler());
+    handlers.put("sum", new SumHandler());
+    handlers.put("count", new CountHandler());
   }
 
   @Override
   protected String handle(HttpServletRequest request) throws Exception {
     String command = request.getParameter("command");
 
-    if ("max".equals(command)) {
+    if (handlers.containsKey(command)) {
+      return handlers.get(command).handle();
+    }
+
+    return template.unknownCommand(command);
+  }
+
+  private interface CommandHandler {
+    String handle() throws Exception;
+  }
+
+  private class MaxHandler implements CommandHandler {
+    @Override
+    public String handle() throws Exception {
       Product product = database.getMaxPrice();
       return template.getMaxPrice(product);
-    } else if ("min".equals(command)) {
+    }
+  }
+
+  private class MinHandler implements CommandHandler {
+    @Override
+    public String handle() throws Exception {
       Product product = database.getMinPrice();
       return template.getMinPrice(product);
-    } else if ("sum".equals(command)) {
+    }
+  }
+
+  private class SumHandler implements CommandHandler {
+    @Override
+    public String handle() throws Exception {
       long sum = database.getSumPrice();
       return template.getSumPrice(sum);
-    } else if ("count".equals(command)) {
+    }
+  }
+
+  private class CountHandler implements CommandHandler {
+    @Override
+    public String handle() throws Exception {
       int count = database.getProductsCount();
       return template.getProductsCount(count);
-    } else {
-      return template.unknownCommand(command);
     }
   }
 }
