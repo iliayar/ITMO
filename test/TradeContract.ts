089e5f4f5e8c206725ded0b833e803adf5a0a512
iliayar <iliayar3@gmail.com>
2022-12-02 23:56:11 +0300

    Initial commit

---
+++ test/TradeContract.ts
@@ -0,0 +1,34 @@
+import { expect } from "chai";
+import { ethers } from "hardhat";
+
+describe("Trade Contract", function () {
+  it("Test transfer", async function () {
+    // Random address with a bunch of WETH
+    const WETH_USER = "0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619";
+    const owner = await ethers.getImpersonatedSigner(WETH_USER);
+    const balance = ethers.utils.parseEther("0.001");
+    const flashloanAmount = ethers.utils.parseEther("0.005");
+
+    const LendingPoolAddressProviderAddress = "0xB53C1a33016B2DC2fF3653530bfF1848a515c8c5";
+    const WETHAddress = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
+
+    const Flashloan = await ethers.getContractFactory("Flashloan");
+    const flashloan = await Flashloan.deploy(LendingPoolAddressProviderAddress, flashloanAmount);
+    await flashloan.deployed()
+
+    const WETH = await ethers.getContractAt("IERC20", WETHAddress);
+
+    const prevBalance = await WETH.balanceOf(owner.address)
+    expect(prevBalance).to.greaterThan(balance);
+    await WETH.connect(owner).transfer(flashloan.address, balance)
+
+    expect(await WETH.balanceOf(owner.address)).to.equal(prevBalance.sub(balance));
+    expect(await WETH.balanceOf(flashloan.address)).to.equal(balance);
+
+    await flashloan.flashloan();
+
+    expect(await WETH.balanceOf(flashloan.address))
+      .to.lessThan(balance)
+      .and.greaterThan(0);
+  })
+});
