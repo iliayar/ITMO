2e53d47227aba4d4e08a2fb66f1a7a61d295a534
iliayar <iliayar3@gmail.com>
2022-12-04 02:32:22 +0300

    feat app: done

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+mod lib;
+
 fn main() {
-    println!("Hello, world!");
+    env_logger::init();
+
+    let app = lib::App::new();
+
+    if let Err(err) = app.start() {
+	eprintln!("{}", err);
+    }
 }
