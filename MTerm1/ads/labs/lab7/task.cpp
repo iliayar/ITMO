6b49261116b8a1f207a3b8869181dcb6dfcb6de3
iliayar <iliayar3@gmail.com>
2025-07-03 21:51:18 +0300

    feat(ads): lab7 done [7/9]

---
+++ MTerm1/ads/labs/lab7/task.cpp
@@ -0,0 +1,159 @@
+
+// Generated at 2025-07-03 00:02:22.913889 
+// By iliayar
+#define _USE_MATH_DEFINES
+#pragma comment(linker, "/STACK:36777216")
+#include <iterator>
+#include <iostream>
+#include <vector>
+#include <chrono>
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+#include <map>
+#include <unordered_set>
+#include <ctime>
+#include <cstdlib>
+#include <queue>
+#include <set>
+#include <deque>
+#include <list>
+#include <sstream>
+#include <numeric>
+#include <optional>
+#include <variant>
+#include <functional>
+
+using namespace std;
+
+#define ON 1
+#define OFF 0
+
+#define int long long
+#ifdef LOCAL
+#define DBG_VAR true
+#define DBG_CODE(x) x
+#else
+#define DBG_VAR false
+#define DBG_CODE(x)
+#endif
+
+#define DBG() if (DBG_VAR) cout << "DBG: "
+
+#define INF 1e+18
+#define ALL(a) a.begin(), a.end()
+
+using vint = vector<int>;
+using vint2 = vector<vint>;
+
+template <typename K, typename V>
+struct map_pair {
+    K k;
+    V v;
+};
+
+template <typename T, typename K>
+ostream &operator<<(ostream &out, pair<T, K> const& p) {
+  out << "(" << p.first << ", " << p.second << ")";
+  return out;
+}
+
+template<typename T>
+class Join {
+  T& v;
+  string const& separator;
+  
+public:
+  
+  Join(T v, string const& separator)
+    : v(v), separator(separator)
+  {}
+
+  friend ostream& operator<<(ostream& out, Join<T> join) {
+    for(auto it = join.v.cbegin(); it != join.v.cend(); ++it) {
+      if(it != join.v.cbegin()) out << join.separator;
+      out << *it;
+    }
+    return out;
+  }
+};
+
+template <typename T>
+ostream &operator<<(ostream &out, vector<T> v) {
+  out << "[" << Join(v, ", ") << "]";
+  return out;
+}
+
+template <typename K, typename V>
+ostream &operator<<(ostream &out, map<K, V> m) {
+  vector<map_pair<K, V>> v;
+  transform(m.begin(), m.end(), back_inserter(v), [](pair<K, V> const& p){return map_pair<K, V>{p.first, p.second};});
+  out << "{" << Join(v, ", ") << "}";
+  return out;
+}
+
+template <typename K, typename V>
+ostream &operator<<(ostream &out, map_pair<K, V> m) {
+  out << m.k << ": " << m.v;
+  return out;
+}
+
+template <typename K>
+ostream &operator<<(ostream &out, set<K> s) {
+  out << "{" << Join(s, ", ") << "}";
+  return out;
+}
+
+//##################CODE BEGIN#############
+//entry
+void sol() {
+    int n1; cin >> n1;
+    vint a1(n1); for (auto& i : a1) cin >> i;
+    int n2; cin >> n2;
+    vint a2(n2); for (auto& i : a2) cin >> i;
+    int n3; cin >> n3;
+    vint a3(n3); for (auto& i : a3) cin >> i;
+
+    vector<vint2> dp(n1 + 1, vint2(n2 + 1, vint(n3 + 1, 0)));
+
+    for (int i = 1; i <= n1; ++i) {
+        for (int j = 1; j <= n2; ++j) {
+            for (int k = 1; k <= n3; ++k) {
+                if (a1[i-1] == a2[j-1] && a2[j-1] == a3[k-1]) {
+                    dp[i][j][k] = max(dp[i][j][k], dp[i-1][j-1][k-1] + 1);
+                }
+                dp[i][j][k] = max(dp[i][j][k], dp[i-1][j][k]);
+                dp[i][j][k] = max(dp[i][j][k], dp[i][j-1][k]);
+                dp[i][j][k] = max(dp[i][j][k], dp[i][j][k-1]);
+            }
+        }
+    }
+    cout << dp[n1][n2][n3] << endl;
+}
+//##################CODE END###############
+#ifdef LOCAL
+#undef FILE_IO
+#undef FILENAME
+#define FILE_IO ON
+#define FILENAME "local"
+#endif
+
+signed main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    #if FILE_IO == ON
+    freopen(FILENAME".in", "r", stdin);
+    freopen(FILENAME".out", "w", stdout);
+    #endif
+    #ifdef LOCAL
+    auto start = std::chrono::high_resolution_clock::now();
+    #endif
+
+    sol();
+
+    #ifdef LOCAL
+    auto stop = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
+    cout << duration.count() << " microseconds" << endl;
+    #endif
+}
---
+++ MTerm1/ads/labs/lab7/taskB1.cpp
@@ -0,0 +1,171 @@
+
+// Generated at 2025-07-01 21:57:41.402030 
+// By iliayar
+#define _USE_MATH_DEFINES
+#pragma comment(linker, "/STACK:36777216")
+#include <iterator>
+#include <iostream>
+#include <vector>
+#include <chrono>
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+#include <map>
+#include <unordered_set>
+#include <ctime>
+#include <cstdlib>
+#include <queue>
+#include <set>
+#include <deque>
+#include <list>
+#include <sstream>
+#include <numeric>
+#include <optional>
+#include <variant>
+#include <functional>
+
+using namespace std;
+
+#define ON 1
+#define OFF 0
+
+#define int long long
+#ifdef LOCAL
+#define DBG_VAR true
+#define DBG_CODE(x) x
+#else
+#define DBG_VAR false
+#define DBG_CODE(x)
+#endif
+
+#define DBG() if (DBG_VAR) cout << "DBG: "
+
+#define INF 1e+18
+#define ALL(a) a.begin(), a.end()
+
+using vint = vector<int>;
+using vint2 = vector<vint>;
+
+template <typename K, typename V>
+struct map_pair {
+    K k;
+    V v;
+};
+
+template <typename T, typename K>
+ostream &operator<<(ostream &out, pair<T, K> const& p) {
+  out << "(" << p.first << ", " << p.second << ")";
+  return out;
+}
+
+template<typename T>
+class Join {
+  T& v;
+  string const& separator;
+  
+public:
+  
+  Join(T v, string const& separator)
+    : v(v), separator(separator)
+  {}
+
+  friend ostream& operator<<(ostream& out, Join<T> join) {
+    for(auto it = join.v.cbegin(); it != join.v.cend(); ++it) {
+      if(it != join.v.cbegin()) out << join.separator;
+      out << *it;
+    }
+    return out;
+  }
+};
+
+template <typename T>
+ostream &operator<<(ostream &out, vector<T> v) {
+  out << "[" << Join(v, ", ") << "]";
+  return out;
+}
+
+template <typename K, typename V>
+ostream &operator<<(ostream &out, map<K, V> m) {
+  vector<map_pair<K, V>> v;
+  transform(m.begin(), m.end(), back_inserter(v), [](pair<K, V> const& p){return map_pair<K, V>{p.first, p.second};});
+  out << "{" << Join(v, ", ") << "}";
+  return out;
+}
+
+template <typename K, typename V>
+ostream &operator<<(ostream &out, map_pair<K, V> m) {
+  out << m.k << ": " << m.v;
+  return out;
+}
+
+template <typename K>
+ostream &operator<<(ostream &out, set<K> s) {
+  out << "{" << Join(s, ", ") << "}";
+  return out;
+}
+
+//##################CODE BEGIN#############
+//entry
+void sol() {
+    int n = 0; cin >> n;
+    vint dp(n + 1, INF);
+    dp[1] = 0;
+    for (int i = 2; i <= n; ++i) {
+        if (i % 2 == 0) {
+            dp[i] = min(dp[i], dp[i / 2] + 1);
+        }
+        if (i % 3 == 0) {
+            dp[i] = min(dp[i], dp[i / 3] + 1);
+        }
+        dp[i] = min(dp[i], dp[i - 1] + 1);
+    }
+
+    vint res;
+    int k = n;
+    res.push_back(k);
+    while (k != 1) {
+        if (k % 2 == 0 && dp[k / 2] == dp[k] - 1) {
+            k = k / 2;
+        } else if (k % 3 == 0 && dp[k / 3] == dp[k] - 1) {
+            k = k / 3;
+        } else if (dp[k - 1] == dp[k] - 1) {
+            k = k - 1;
+        } else {
+            exit(322);
+        }
+        res.push_back(k);
+    }
+
+    cout << dp[n] << endl;
+    for (int i = res.size() - 1; i >= 0; --i) {
+        cout << res[i] << " ";
+    }
+    cout << endl;
+}
+//##################CODE END###############
+#ifdef LOCAL
+#undef FILE_IO
+#undef FILENAME
+#define FILE_IO ON
+#define FILENAME "local"
+#endif
+
+signed main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    #if FILE_IO == ON
+    freopen(FILENAME".in", "r", stdin);
+    freopen(FILENAME".out", "w", stdout);
+    #endif
+    #ifdef LOCAL
+    auto start = std::chrono::high_resolution_clock::now();
+    #endif
+
+    sol();
+
+    #ifdef LOCAL
+    auto stop = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
+    cout << duration.count() << " microseconds" << endl;
+    #endif
+}
