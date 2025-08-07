a5191eab65086634c40190c0282f07d6570f00fd
iliayar <iliayar3@gmail.com>
2025-08-08 01:18:03 +0300

    feat(ads): lab6 done [5/8]

--- MTerm1/ads/labs/lab6/sol.cpp
+++ MTerm1/ads/labs/lab6/sol.cpp
@@ -1,57 +1,6 @@
 #include "template.cpp"
 
-struct S {
-    int i;
-    int j;
-    int c;
-};
-
 //entry
 void sol() {
-    int n; cin >> n;
-    vint a(n);
-    for (int i = 0; i < n; ++i) {
-        cin >> a[i];
-    }
 
-    int i = 0, j = 1;
-
-    int s = 0;
-    vector<S> p(n + 1, 0);
-    for (int i = n - 1; ++i) {
-        if (p[i + 1] > a[i]) {
-            p[i] = p[i + 1] - a[i];
-        } else {
-            p[i] = a[i] - p[i + 1]
-        }
-        s += a[i];
-    }
-
-    vector<S> res;
-    // while (i < n && j < n) {
-    //     int c = min(a[i], a[j]);
-    //     res.push_back({i, j, c});
-    //     a[i] -= c;
-    //     a[j] -= c;
-    //
-    //     while (a[i] == 0 && i < n) i++;
-    //     while (a[j] == 0 && j < n) j++;
-    //
-    //     if (i == j) {
-    //         j += 1;
-    //     } else if (j < i) {
-    //         swap(j, i);
-    //     }
-    // }
-    for (int i = 0; i < n; ++i) {
-        int j = i + 1;
-        for (;j < n; ++j) {
-
-        }
-    }
-
-    cout << res.size() << endl;
-    for (int i = 0; i < res.size(); ++i) {
-        cout << res[i].i + 1 << " " << res[i].j + 1 << " " << res[i].c << endl;
-    }
 }
