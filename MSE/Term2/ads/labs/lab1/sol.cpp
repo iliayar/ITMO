de9ec6f4331379307723458d0c4efc60909b2c09
iliayar <iliayar3@gmail.com>
2026-02-13 22:40:37 +0300

    feat(ads2): lab 1

--- MSE/Term2/ads/labs/lab1/sol.cpp
+++ MSE/Term2/ads/labs/lab1/sol.cpp
@@ -5,6 +5,170 @@ struct R {
     int diff;
 };
 
+struct llong;
+ostream& operator<<(ostream& out, llong const& n);
+
+struct llong {
+    constexpr static long long SIZE = 128;
+    constexpr static long long BYTE = 1 << 16;
+    long long d[SIZE];
+    bool neg;
+
+    llong(int n) {
+        // DBG() << "llong(" << n << ")" << endl;
+        neg = false;
+        if (n < 0) {
+            neg = true;
+            n = -n;
+        }
+        for (int i = 0; i < SIZE; ++i) {
+            d[i] = 0;
+        }
+        int i = 0;
+        while (n > 0) {
+            d[i++] = n % BYTE; 
+            n /= BYTE;
+        }
+    }
+
+    llong add_mod(llong const& other) const {
+        // DBG() << "add_mod" << endl;
+        llong res = *this;
+        res.neg = false;
+        for (int i = 0; i < SIZE; ++i) {
+            res.d[i] += other.d[i];
+            while (res.d[i] >= BYTE) {
+                if (i + 1 >= SIZE) {
+                    DBG() << "Overflow while adding " << *this << " + " << other << endl;
+                    exit(67);
+                }
+                res.d[i] -= BYTE;
+                res.d[i + 1] += 1;
+            }
+        }
+        return res;
+    }
+
+    llong sub_mod(llong const& other) const {
+        // DBG() << "sub_mod" << endl;
+        if (other.gt_mod(*this)) {
+            auto res = other.sub_mod(*this);
+            res.neg = !res.neg;
+            return res;
+        }
+        llong res = *this;
+        res.neg = false;
+        for (int i = 0; i < SIZE; ++i) {
+            res.d[i] -= other.d[i];
+            while (res.d[i] < 0) {
+                if (i + 1 >= SIZE) exit(67);
+                res.d[i] += BYTE;
+                res.d[i + 1] -= 1;
+            }
+
+        }
+        return res;
+    }
+
+    bool gt_mod(llong const& other) const {
+        for (int i = SIZE - 1; i >= 0; --i) {
+            if (d[i] > other.d[i]) {
+                return true;
+            } else if (d[i] < other.d[i]) {
+                return false;
+            }
+        }
+        return false;
+    }
+
+    llong operator+(llong const& other) const {
+        // DBG() << "operator+" << endl;
+        if (neg && other.neg) {
+            auto res = add_mod(other);
+            res.neg = true;
+            return res;
+        } else if (neg && !other.neg) {
+            return other.sub_mod(*this);
+        } else if (!neg && other.neg) {
+            return sub_mod(other);
+        } else {
+            return add_mod(other);
+        }
+    }
+
+    llong& operator+=(llong const& other) {
+        *this = *this + other;
+        return *this;
+    }
+
+    llong operator-(llong const& other) const {
+        if (neg && other.neg) {
+            return other.sub_mod(*this);
+        } else if (neg && !other.neg) {
+            auto res = add_mod(other);
+            res.neg = true;
+            return res;
+        } else if (!neg && other.neg) {
+            return add_mod(other);
+        } else {
+            return sub_mod(other);
+        }
+    }
+
+    llong operator-() const {
+        llong res = *this;
+        res.neg = !res.neg;
+        return res;
+    }
+
+    bool operator<(llong const& other) const {
+        if (neg && other.neg) {
+            return gt_mod(other);
+        } else if (neg && !other.neg) {
+            return true;
+        } else if (!neg && other.neg) {
+            return false;
+        } else {
+            return other.gt_mod(*this);
+        }
+    }
+
+    bool operator==(llong const& other) const {
+        bool is_zero = true;
+        for (int i = 0; i < SIZE; ++i) {
+            if (d[i] != other.d[i]) {
+                return false;
+            }
+            if (d[i] != 0) {
+                is_zero = false;
+            }
+        }
+
+        if (is_zero) return true;
+        if (other.neg == neg) return true;
+        return false;
+    }
+
+    static llong inf() {
+        llong res(0);
+        for (int i = 0; i < SIZE; ++i) {
+            res.d[i] = BYTE - 1;
+        }
+        return res;
+    }
+};
+
+ostream& operator<<(ostream& out, llong const& n) {
+    if (n.neg) cout << "-";
+    for (int i = llong::SIZE - 1; i >= 0; --i) {
+        out << n.d[i];
+        if (i != 0) {
+            cout << ":";
+        }
+    }
+    return out;
+}
+
 void sol_case(int n, int m) {
     vector<vector<R>> g(n, vector<R>());
     for (int i = 0; i < m; ++i) {
@@ -78,52 +242,49 @@ void sol_case(int n, int m) {
             }
         }
     }
-    DBG() << reachable << endl;
+    // DBG() << reachable << endl;
 
-    vector<double> d(n, -numeric_limits<double>::infinity());
+    auto neginf = -llong::inf();
+    vector<llong> d(n, neginf);
     for (int i = 0; i < n; ++i) {
         for (auto& r : g[i]) {
             if (r.right.empty()) {
-                if (r.diff > d[i]) {
+                if (d[i] < llong(r.diff)) {
                     d[i] = r.diff;
                 }
             }
         }
     }
-    double ns = 1.;
-    double nn = ns;
-    for (int i = 0; i < n; ++i, nn *= ns) {
-        vector<double> dt(n, -numeric_limits<double>::infinity());
+    // DBG() << d << endl;
+    for (int i = 0; i < n; ++i) {
         for (int l = 0; l < n; ++l) {
             if (!finite[l]) continue;
-            dt[l] = max(dt[l], d[l] / ns);
             for (auto& r : g[l]) {
                 bool f = false;
-                double dc = r.diff / nn;
+                llong dc = r.diff;
                 for (auto& nt : r.right) {
-                    if (!finite[nt]) {
+                    if (!finite[nt] || d[nt] == neginf) {
                         f = true;
                         break;
                     }
-                    dc += d[nt] / ns;
+                    dc += d[nt];
                 }
                 if (f) continue;
-                if (dt[l] < dc) {
-                    dt[l] = dc;
+                if (d[l] < dc) {
+                    d[l] = dc;
                 }
             }
         }
-        swap(dt, d);
+        // DBG() << d << endl;
     }
-    nn /= ns;
 
     for (int l = 0; l < n; ++l) {
         if (!finite[l] || !reachable[l]) continue;
         for (auto& r : g[l]) {
             bool f = false;
-            double dc = r.diff / nn;
+            llong dc = r.diff;
             for (auto& nt : r.right) {
-                if (!finite[nt]) {
+                if (!finite[nt] || d[nt] == neginf) {
                     f = true;
                     break;
                 }
@@ -136,9 +297,9 @@ void sol_case(int n, int m) {
             }
         }
     }
-    DBG() << d << endl;
+    // DBG() << d << endl;
 
-    if (d[0] > 0) {
+    if (llong(0) < d[0]) {
         cout << "YES" << endl;
     } else {
         cout << "NO" << endl;
