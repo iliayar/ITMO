0c87faacc5ea41dd23a84d8a6a89adbbb84fa22e
iliayar <iliayar3@gmail.com>
2026-04-04 14:09:48 +0300

    feat(ads2): lab 8

---
+++ MSE/Term2/ads/labs/lab8/taskC2.cpp
@@ -0,0 +1,183 @@
+
+// Generated at 2026-04-05 01:08:33.468230 
+// By iliayar
+#define _USE_MATH_DEFINES
+#pragma comment(linker, "/STACK:36777216")
+#include <iterator>
+#include <iostream>
+#include <vector>
+#include <chrono>
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+#include <map>
+#include <unordered_set>
+#include <ctime>
+#include <cstdlib>
+#include <queue>
+#include <set>
+#include <deque>
+#include <list>
+#include <sstream>
+#include <numeric>
+#include <optional>
+#include <variant>
+#include <functional>
+#include <bitset>
+#include <memory>
+
+using namespace std;
+
+#define ON 1
+#define OFF 0
+
+#define int long long
+#ifdef LOCAL
+#define DBG_VAR true
+#define DBG_CODE(x) x
+#else
+#define DBG_VAR false
+#define DBG_CODE(x)
+#endif
+
+#define DBG() if (DBG_VAR) cout << "DBG: "
+
+#define INF 1e+18
+#define ALL(a) a.begin(), a.end()
+
+#define FUNC(retTy, name, args...) std::function<retTy (args)> name = [&](args) -> retTy
+
+using vint = vector<int>;
+using vint2 = vector<vint>;
+
+template <typename K, typename V>
+struct map_pair {
+    K k;
+    V v;
+};
+
+template <typename T, typename K>
+ostream &operator<<(ostream &out, pair<T, K> const& p) {
+  out << "(" << p.first << ", " << p.second << ")";
+  return out;
+}
+
+template<typename T>
+class Join {
+  T& v;
+  string const& separator;
+  
+public:
+  
+  Join(T v, string const& separator)
+    : v(v), separator(separator)
+  {}
+
+  friend ostream& operator<<(ostream& out, Join<T> join) {
+    for(auto it = join.v.cbegin(); it != join.v.cend(); ++it) {
+      if(it != join.v.cbegin()) out << join.separator;
+      out << *it;
+    }
+    return out;
+  }
+};
+
+template <typename T>
+ostream &operator<<(ostream &out, vector<T> v) {
+  out << "[" << Join(v, ", ") << "]";
+  return out;
+}
+
+template <typename K, typename V>
+ostream &operator<<(ostream &out, map<K, V> m) {
+  vector<map_pair<K, V>> v;
+  transform(m.begin(), m.end(), back_inserter(v), [](pair<K, V> const& p){return map_pair<K, V>{p.first, p.second};});
+  out << "{" << Join(v, ", ") << "}";
+  return out;
+}
+
+template <typename K, typename V>
+ostream &operator<<(ostream &out, unordered_map<K, V> m) {
+  vector<map_pair<K, V>> v;
+  transform(m.begin(), m.end(), back_inserter(v), [](pair<K, V> const& p){return map_pair<K, V>{p.first, p.second};});
+  out << "{" << Join(v, ", ") << "}";
+  return out;
+}
+
+template <typename K, typename V>
+ostream &operator<<(ostream &out, map_pair<K, V> m) {
+  out << m.k << ": " << m.v;
+  return out;
+}
+
+template <typename K>
+ostream &operator<<(ostream &out, set<K> s) {
+  out << "{" << Join(s, ", ") << "}";
+  return out;
+}
+
+std::function<void()> finish = [](){ exit(0); };
+
+//##################CODE BEGIN#############
+vint sieve(sqrt(2000000000) + 2);
+
+int phi(int n) {
+    int res = n;
+    for (int i = 2, end = sqrt(n); i <= end; ++i) {
+        if (sieve[i]) continue;
+        if (n % i != 0) continue;
+        res = res / i * (i - 1);
+
+        while (n % i == 0) {
+            n /= i;
+        }
+    }
+    if (n != 1) {
+        res = res / n * (n - 1);
+    }
+    return res;
+}
+
+//entry
+void sol() {
+    sieve[0] = 1;
+    sieve[1] = 1;
+    for (int i = 2; i < sieve.size(); i++) {
+        for (int j = i * 2; j < sieve.size(); j += i) {
+            sieve[j] = 1;
+        }
+    }
+
+    int n;
+    while (cin >> n) {
+        cout << phi(n) << endl;
+    }
+}
+//##################CODE END###############
+#ifdef LOCAL
+#undef FILE_IO
+#undef FILENAME
+#define FILE_IO ON
+#define FILENAME "local"
+#endif
+
+signed main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    #if FILE_IO == ON
+    freopen(FILENAME".in", "r", stdin);
+    freopen(FILENAME".out", "w", stdout);
+    #endif
+    #ifdef LOCAL
+    auto start = std::chrono::high_resolution_clock::now();
+    finish = [&]() {
+        auto stop = std::chrono::high_resolution_clock::now();
+        auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
+        cout << duration.count() << " microseconds" << endl;
+        exit(0);
+    };
+    #endif
+
+    sol();
+    finish();
+}
