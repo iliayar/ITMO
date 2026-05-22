e372905c3edef1eba41d20db0942a296a6bb1474
iliayar <iliayar3@gmail.com>
2026-05-22 23:46:17 +0300

    feat(ads): lab 13

---
+++ MSE/Term2/ads/labs/lab13/Program.fs
@@ -0,0 +1,24 @@
+﻿open System
+
+let main =
+    let s = Console.ReadLine()
+
+    let cmp i j =
+        let mutable k = 0
+
+        while max i j + k < s.Length && s[i + k] = s[j + k] do
+            k <- k + 1
+
+        if max i j + k = s.Length then
+            if i < j then 1
+            else if i = j then 0
+            else -1
+        else if s[i + k] < s[j + k] then
+            -1
+        else if s[i + k] = s[j + k] then
+            0
+        else
+            1
+
+    let res = List.sortWith cmp (List.init s.Length id)
+    printfn "%s" (String.concat " " (List.map (fun i -> string (i + 1)) res))
